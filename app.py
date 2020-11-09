73b6b7a63c47ccd2084afc363d9e04330c4b8006
Admin <fredsmith1970>
2020-11-09 15:52:18 +0000

    abc route

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello everyone !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
+@app.route('/abc')
+def abc():
+    return 'Hello from ABC'
+
 if __name__ == '__main__':
     app.run()
