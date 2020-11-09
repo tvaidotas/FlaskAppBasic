7154955c158f141e4a434afcab61df003ee6e45d
Andrew (QA Apprenticeships) <andrew.burgess@qa.com>
2020-11-09 15:26:11 +0000

    xyz

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello everyone !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
+@app.route('/xyz')
+def xyz():
+    return 'Hello from XYZ'
 
 if __name__ == '__main__':
     app.run()
