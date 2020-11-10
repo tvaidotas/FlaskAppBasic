66dc6797af3516d1f0b7214080bb17e67c67a674
Andrew (QA Apprenticeships) <andrew.burgess@qa.com>
2020-11-10 12:03:35 +0000

    reset

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 from flask import Flask
 
-# Comment from Andrew...
-# Another comment...
-# And another one!
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello everyone !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-
-@app.route('/abc')
-def abc():
-    return 'Hello from ABC'
-
-@app.route('/xyz')
-def xyz():
-    return 'Hello from XYZ'
 
 if __name__ == '__main__':
     app.run()
