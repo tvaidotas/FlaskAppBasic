9ad36f323868d32596b95fcade6e09b959a83704
qa-apprenticeships <72618695+qa-apprenticeships@users.noreply.github.com>
2021-04-13 14:31:43 +0100

    Upstream change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello everyone !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    return 'Hello everyone I just changed!'
 
 if __name__ == '__main__':
     app.run()
