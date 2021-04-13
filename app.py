2e0cd130baf27295afa73bf5a9c3c319f7c21c51
fredsmith1970 <68619136+fredsmith1970@users.noreply.github.com>
2021-04-13 15:02:46 +0100

    Incorporate my name in the response (#2)
    * my name
    
    * better

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello everyone I just changed!'
+    return 'Hello everyone from Andrew Burgess'
 
 if __name__ == '__main__':
     app.run()
