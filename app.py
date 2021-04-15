44ff7b6691ab88aecbbf714c60d7f2260670a115
lake17 <danmead01@hotmail.co.uk>
2021-04-15 12:53:17 +0100

    Change of name by Dan

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello everyone from Andrew Burgess'
+    return 'Hello everyone from Dan Mead'
 
 if __name__ == '__main__':
     app.run()
