f4e06cd9fe76a7318461637cf06dbfba6c967509
pedcortez <cortez.cps@gmail.com>
2021-11-11 12:21:00 +0000

    app.py updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello everyone !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    return 'Hello everyone !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
 
 if __name__ == '__main__':
